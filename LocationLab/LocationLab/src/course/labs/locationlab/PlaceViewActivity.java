8f729835dcfc43de63121c63c0d1cac027e7c56e
heroined <heroinedor@gmail.com>
2014-03-06 08:05:58 +0100

    premières modifs

--- LocationLab/LocationLab/src/course/labs/locationlab/PlaceViewActivity.java
+++ LocationLab/LocationLab/src/course/labs/locationlab/PlaceViewActivity.java
@@ -4,17 +4,20 @@ import java.util.ArrayList;
 
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class PlaceViewActivity extends ListActivity implements LocationListener {
@@ -42,11 +45,13 @@ public class PlaceViewActivity extends ListActivity implements LocationListener 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-
+		super.onCreate(savedInstanceState);
+		
 		// TODO - Set up the app's user interface
 		// This class is a ListActivity, so it has its own ListView 
 		// ListView's adapter should be a PlaceViewAdapter
-
+		mAdapter = new PlaceViewAdapter(getApplicationContext());
+		setListAdapter(mAdapter);
 
 		// TODO - add a footerView to the ListView
 		// You can use footer_view.xml to define the footer
@@ -56,7 +61,20 @@ public class PlaceViewActivity extends ListActivity implements LocationListener 
 		// 2) The current location has been seen before - issue Toast message
 		// 3) There is no current location - response is up to you. The best
 		// solution is to disable the footerView until you have a location.
+		LayoutInflater inflater = getLayoutInflater();
+		TextView footerView = (TextView) inflater.inflate(R.layout.footer_view, null, false);
+		getListView().addFooterView(footerView);
 
+		footerView.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+//				if (is)
+//				
+//				Intent addToDoIntent = new Intent( ToDoManagerActivity.this ,AddToDoActivity.class);
+//				startActivityForResult(addToDoIntent, ADD_TODO_ITEM_REQUEST);
+
+			}
+		});
 
 	}
 
